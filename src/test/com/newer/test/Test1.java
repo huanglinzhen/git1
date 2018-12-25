05c712d9b7b8561c71fbe89e01a9e41a6d6668c8
huanglinzhen <2825402175@qq.com>
2018-12-25 23:00:02 +0800

    Initial commit

---
+++ src/test/com/newer/test/Test1.java
@@ -0,0 +1,21 @@
+package com.newer.test;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void test1(){
+        System.out.println("111");
+    }
+
+    @Test
+    public  void  test2(){
+        System.out.println("这是jy写的代码阿萨德");
+    }
+    @Test
+    public void test3(){
+        System.out.println("热爱生活");
+    }
+
+}
+
